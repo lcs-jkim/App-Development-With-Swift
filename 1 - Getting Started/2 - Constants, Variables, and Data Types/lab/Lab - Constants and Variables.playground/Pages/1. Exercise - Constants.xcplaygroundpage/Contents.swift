07f4cd4d4d93c3fe168931cbd94a99124acd3651
Amanda <jkim@lcs.on.ca>
2019-09-30 16:00:27 -0400

    finished lab 2

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -10,6 +10,6 @@ print (friends)
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
  */
-print
+print (friends)
 
 //: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
